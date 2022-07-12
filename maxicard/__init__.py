0df1923066549539f231c7791f44c4ff0845ebb1
Maxi_TM <82947033+Maxi-TM@users.noreply.github.com>
2022-07-12 12:21:04 +0200

    Update __init__.py

--- maxicard/__init__.py
+++ maxicard/__init__.py
@@ -17,7 +17,7 @@ class WelcomeCard():
         font_directory = os.path.join(os.path.dirname(__file__), "fonts")
         font_path = os.path.join(font_directory, "SFMono.ttf")
 
-        sfmono = Font(font_path, size=21)
+        sfmono = Font(font_path, size=22)
         background.text(
             (301, 13),
             f"{self.member.id}",
