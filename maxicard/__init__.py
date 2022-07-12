839abdc630ea8e3fdb360404a0fe01eb324006b9
Maxi_TM <82947033+Maxi-TM@users.noreply.github.com>
2022-07-12 14:30:43 +0200

    Update __init__.py

--- maxicard/__init__.py
+++ maxicard/__init__.py
@@ -28,7 +28,7 @@ class WelcomeCard():
             color="#c1c1c1",
         )
         background.text(
-            (159, 44),
+            (159, 38),
             f"{self.member}",
             font=sfmono,
             color="#c1c1c1",
