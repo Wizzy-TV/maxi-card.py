6a37ad27ba799e9405897fb286a0fb0797e125de
Maxi_TM <82947033+Maxi-TM@users.noreply.github.com>
2022-07-14 11:14:30 +0200

    Added LeaveCard

--- maxicard/__init__.py
+++ maxicard/__init__.py
@@ -74,3 +74,76 @@ class WelcomeCard():
 
         file = File(fp=background.image_bytes, filename="card.png")
         return file
+
+class LeaveCard():
+
+    def __init__(self):
+        self.member : str = None
+        self.datetime : str = None
+        self.server : str = None
+
+    async def create(self):  
+        bgc = await load_image_async('https://raw.githubusercontent.com/Maxi-TM/maxi-card.py/main/maxicard/imgs/bg-welcome.png')
+        background = Editor(bgc).resize((552, 156))
+
+        font_directory = os.path.join(os.path.dirname(__file__), "fonts")
+        font_path = os.path.join(font_directory, "SFMono.ttf")
+
+        sfmono = Font(font_path, size=22)
+        background.text(
+            (159, 12),
+            f"fetch {self.member.id}",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+        background.text(
+            (16, 44),
+            f"username",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+        background.text(
+            (159, 38),
+            f"{self.member}",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+        background.text(
+            (16, 67),
+            f"created",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+        background.text(
+            (159, 67),
+            f"{self.datetime}",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+        background.text(
+            (16, 98),
+            f"status",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+        background.text(
+            (159, 94),
+            f"Left the server",
+            font=sfmono,
+            color="#e44b3b",
+        )
+        background.text(
+            (16, 123),
+            f"members",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+        background.text(
+            (159, 124),
+            f"{self.server.member_count}",
+            font=sfmono,
+            color="#c1c1c1",
+        )
+
+        file = File(fp=background.image_bytes, filename="card.png")
+        return file
