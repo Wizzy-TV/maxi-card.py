36a844b5bac0729b4efe66e0be71ac35040ad3dc
Maxi_TM <82947033+Maxi-TM@users.noreply.github.com>
2022-07-18 15:47:53 +0200

    Update __init__.py

--- maxicard/__init__.py
+++ maxicard/__init__.py
@@ -158,7 +158,7 @@ class DeleteCard():
         background = Editor(bgc).resize((1280, 609))
         if(self.avatar != None):
             profile = await load_image_async(str(self.avatar))
-            profile = Editor(profile).resize((347, 347))
+            profile = Editor(profile).resize((337, 337))
             background.paste(profile.image, (225, 230))
         
         file = File(fp=background.image_bytes, filename="card.png")
