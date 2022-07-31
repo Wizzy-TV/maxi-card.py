0501674f9484fc66d3a9c57df2fb5b7144021564
Maxi_TM <82947033+Maxi-TM@users.noreply.github.com>
2022-07-31 12:00:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name='maxi-card.py',
-  version='1.0.1',
+  version='1.0.2',
   description='This is simple maker for cards welcome and leave in discord bot.',
   long_description=open('README.rst').read(),
   url='',  
